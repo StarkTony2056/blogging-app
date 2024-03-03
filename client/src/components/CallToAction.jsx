568ee803c98f5228d8a095b90b2bd389b43474b3
StarkTony2056 <chakrabortymainak03@gmail.com>
2024-03-03 21:25:14 +0530

    Complete about and projects pages

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,8 +5,8 @@ export default function CallToAction() {
   return (
     <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
         <div className='flex-1 justify-center flex flex-col'>
-            <h2 className='text-2xl'>Want to learn more about machine learning?</h2>
-            <p className='text-gray-500 my-2'>Check out these resources on my github page!</p>
+            <h2 className='text-2xl'>Want to learn more about my projects?</h2>
+            <p className='text-gray-500 my-2'>Check out my github page!</p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
                 <a href='https://github.com/StarkTony2056' target='_blank' rel='noopener noreferrer'>Learn More</a>
             </Button>
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className='font-semibold text-3xl text-center my-7'>About Mainak's Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Mainak's Blog is a blog webapp that I created to share my ideas and insights in the field of computer science and software engineering.<br />
+              I am a computer science student doing a specialization in information technology and machine learning systems, and I want to write about my experiences in the field of education and research.<br />
+              I hope you enjoy reading my blog!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
