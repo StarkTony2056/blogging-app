1c0c5a17dcfd650c9bcdcc529b309a0da38a6c32
Mainak Chakraborty <chakrabortymainak03@gmail.com>
2024-04-06 10:06:54 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -51,8 +51,8 @@ export default function Header() {
   return (
     <Navbar className='border-b-2'>
       <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-          <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Mainak's</span>
-          Blog
+          <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Blog</span>
+          ify
       </Link>
       <form onSubmit={handleSubmit}>
         <TextInput type='text' placeholder='Search...' rightIcon={AiOutlineSearch} value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className='hidden lg:inline'/>
